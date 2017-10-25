032244787cd581f5c728343244d2cf6aa267c9ec
Jehorn <gerardgu@outlook.com>
2017-10-25 18:58:47 +0800

    new carousel about finished.

--- 02_demos/carousel/carousel-pro.js
+++ 02_demos/carousel/carousel-pro.js
@@ -10,6 +10,22 @@
 
 		// 原始图片路径数组
 		obj.datas     = options.datas;
+		// 渲染超过个数 默认为 1 表示首尾各多添加一个 最小为 1 最大为 obj.datas.length
+		obj.overCount = parseInt(options.overCount) || 1;
+		obj.overCount < 1 ? obj.overCount = 1 : obj.overCount;
+		obj.overCount > obj.datas.length ? obj.overCount = obj.datas.length : obj.overCount;
+		// 渲染数组
+		obj.renderDatas = [];
+		for (var data in obj.datas) {
+			obj.renderDatas.push(obj.datas[data]);
+		}
+		for(var c = 0; c < obj.overCount; c++) {
+			var prev        = obj.datas[obj.datas.length - (1 + c)];
+			var next        = obj.datas[0 + c];
+			obj.renderDatas.unshift(prev);
+			obj.renderDatas.push(next);
+		}
+
 		// 创建文字轮播 将会创建指定宽高背景色内容的轮播
 		// 设置此项将会使轮播以字符串显示 datas 数组中的内容
 		// 必须设置为布尔值 ture 起效
@@ -37,6 +53,8 @@
 
 		// 初始化显示图片
 		obj.current     = options.index === undefined ? 0 : parseInt(options.index);
+		obj.current < 0 ? obj.current = 0 : '';
+		obj.current > obj.datas.length - 1 ? obj.current = obj.datas.length - 1 : '';
 		// 分页容器id
 		obj.isPager     = options.pager;
 		// 分页是否具有控制功能
@@ -73,18 +91,20 @@
 		obj.isAuto      = options.auto === undefined ? true : options.auto;
 		// 是否鼠标悬浮轮播上时停止播放
 		obj.hoverStop   = options.hoverStop === undefined ? true : options.hoverStop;
-		// 播放速度(每张图片停留时间)
+		// 播放速度(每张图片停留时间) 最小值为 1
 		obj.speed       = options.speed || 1200;
+		obj.speed < 1 ? obj.speed = 1 : obj.speed;
 		// 过度效果速度(图片切换动画速度)
 		obj.animate     = options.animate || 600;
+		obj.animate < 1 ? obj.animate = 1 : obj.animate;
 		// 过渡效果类型(slide|fade)
 		obj.animateType = options.animateType || 'slide';
 		// 图片轮播方向自(left|right)
 		obj.direction   = options.direction || 'right';
 		// 轮播高度
-		obj.height      = options.height || 400;
+		obj.height      = options.height || 300;
 		// 图片高度 请自行指定单位
-		obj.imgHeight   = options.imgHeight || 'auto';
+		obj.imgHeight   = options.imgHeight || '100%';
 		// 图片宽度 请自行指定单位
 		obj.imgWidth    = options.imgWidth || '100%';
 		// 轮播容器外边距
@@ -96,7 +116,7 @@
 		// 轮播按钮向后id
 		obj.next        = options.next;
 		// 轮播初始化显示延时
-		obj.delay       = options.delay || 30;
+		obj.delay       = options.delay || 300;
 		// 轮播容器DOM
 		obj.container   = options.container;
 		// 轮播元素(用于清空)
@@ -107,6 +127,8 @@
 		obj.hrefName    = options.hrefName || 'href';
 		// 如果输对象数组 且需要不同背景 请指定背景属性名称 default: background
 		obj.backgroundName = options.backgroundName || 'background';
+		// 滑动效果动画曲线
+		obj.slideTimingFunction = options.slideTimingFunction || 'ease';
 
 
 		return obj;
@@ -126,21 +148,526 @@
 		return obj;
 	}
 
+	// 验证浏览器对 transition 的兼容
+	var transitionEvent = function () {
+       	var el = document.createElement('surface'),
+       	transitions = {
+         	'transition':'transitionend',
+         	'OTransition':'oTransitionEnd',
+         	'MozTransition':'transitionend',
+         	'WebkitTransition':'webkitTransitionEnd'
+       	}
+
+       	for(var t in transitions){
+          	if( el.style[t] !== undefined ){
+               	return transitions[t];
+           	}
+       	}
+   	}
+
+	// 清除轮播容器内多余的元素
+	// @param {object} container 轮播容器
+	// @param {array}  items     多余轮播元素 .carousel-item
+	// @return 					 void
+	var clear = function (container, items) {
+		for (var i = items.length - 1; i >= 0; i--) {
+			container.removeChild(items[i]);
+		}
+	}
+
+	// 动画效果 基类
+	var Animation = function (self, index) {
+		var crt          = self.watchObj.current;
+		var datas        = self.datas;
+		this.box         = self.container;
+		this.speed       = self.opts.animate;
+		this.realLeftLen = self._initLeft - self._multiple * (index);
+		this.leftLen     = this.realLeftLen;
+		index            = parseInt(index);
+
+		if (crt === 0 && index === datas.length - 1) {
+			// 当前为第一个 将要切换的为最后一个
+			// 即为从第一个向最后一个切换
+			this.leftLen = self.left + self._multiple;
+		} else if (crt === datas.length - 1 && index === 0) {
+			// 即为从最后一个向第一个切换
+			this.leftLen = self.left - self._multiple;
+		}
+	}
+
+	// 滑动效果
+	// @param  {Object} self  轮播类 this 指针
+	// @param  {Number} index 将要改变到的下标(相对于原始数组而言)
+	// @return {void}
+	var SlideAnimate = function (self, index) {
+		Animation.call(this, self, index);
+
+		var _this = this;
+
+		this.box.style.transitionDuration       = this.speed + 'ms';
+		this.box.style.transitionTimingFunction = self.opts.slideTimingFunction;
+		this.box.style.transform                = 'translate3d(' + this.leftLen + '%, 0, 0)';
+
+		// 监听动画事件完成
+		var transition = transitionEvent();
+		transition && this.box.addEventListener(transition, function() {
+			// 销毁事件
+			_this.box.removeEventListener(transition, arguments.callee, false);
+			_this.box.style.transitionDuration = '0ms';
+			_this.box.style.transform          = 'translate3d(' + _this.realLeftLen + '%, 0, 0)';
+			self.left                          = _this.realLeftLen;
+			self.watchObj.current              = index;
+	   	});
+	}
+
+	// 淡入淡出效果
+	// @param  {Object} self  轮播类 this 指针
+	// @param  {Number} index 将要改变到的下标(相对于原始数组而言)
+	// @return {void}
+	var FadeAnimate = function (self, index) {
+		Animation.call(this, self, index);
+
+		var _this = this;
+		var speed = this.speed / 3;
+
+		this.box.style.transition = 'opacity ' + (speed * 1 + 'ms ') + 'ease-out';
+		this.box.style.opacity = 0.1;
+
+		// 监听动画事件完成
+		var transition = transitionEvent();
+		transition && this.box.addEventListener(transition, function() {
+			// 销毁事件
+			_this.box.removeEventListener('animationend', arguments.callee, false);
+			_this.box.style.transition = 'opacity ' + (speed * 2 + 'ms ') + 'ease-in';
+			_this.box.style.transform = 'translate3d(' + _this.realLeftLen + '%, 0, 0)';
+
+			_this.box.style.opacity = 1;
+
+			self.left             = _this.realLeftLen;
+			self.watchObj.current = index;
+		});
+	}
+
+	// 轮播容器处理
+	// @param {object} container 轮播容器
+	// @param {object} opt       轮播容器设置参数
+	// @return                   void
+	var setContainer = function (container, opt) {
+		opt = opt || {};
+
+		clear(container, opt.items);
+
+		var getValue = function (value) {
+			var val;
+			typeof value === 'number' ? val = value + 'px' : val = value;
+			return val;
+		}
+
+		container.style.height     = getValue(opt.height);
+		container.style.margin     = getValue(opt.margin);
+		container.style.padding    = getValue(opt.padding);
+		container.style.clear      = 'both';
+		container.style.overflow   = 'hidden';
+		container.style.listStyle  = 'none';
+		container.style.visibility = 'hidden';
+		container.style.opacity    = '0';
+		container.className        = 'carousel';
+	}
+
+	// 创建轮播元素
+	// @param  {object} self 调用方的this
+	// @param  {object} opt  轮播元素参数
+	// @return {object} item 轮播元素
+	var createItem = function (self, opt) {
+		var isText    = self.opts.isText;
+		var textStyle = self.opts.textStyle;
+		var href      = opt.href || 'javascript: void(0);';
+		var bg        = opt.background || self.opts.background;
+		var item      = document.createElement('a');
+		var img       = document.createElement('img');
+		var width     = 100 / self.renderDatas.length + '%';
+
+		item.className        = 'carousel-item';
+		item.style.width      = width;
+		item.style.background = bg;
+		item.href             = href;
+		item.style.display    = 'block';
+		item.style.float      = 'left';
+		item.style.height     = '100%';
+		item.style.border     = '0';
+
+		img.style.width  = self.opts.imgWidth;
+		img.style.height = self.opts.imgHeight;
+		if (isText === true) {
+			// 如果是显示文字 此时src即为文字内容
+			img           = document.createElement('div');
+			img.innerHTML = opt.src;
+			for(var style in textStyle) {
+				item.style[style] = textStyle[style];
+			}
+		} else {
+			// 显示图片
+			img.src = opt.src;
+			try {
+				img.alt = opt.alt || opt.src.split('/')[opt.src.split('/').length - 1];
+			} catch (e) {
+				console.warn(e);
+			}
+		}
+
+		item.appendChild(img);
+
+		return item;
+	}
+
+	// 渲染元素
+	// @param  {object} self 调用方的this
+	// @return {void}
+	var render = function (self) {
+		var renderDatas = self.renderDatas;
+
+		clear(self.container, self.container.childNodes);
+		for (var i = 0; i < renderDatas.length; i++) {
+			var node, opt = {};
+			if (typeof renderDatas[i] === 'string') {
+				opt.src = renderDatas[i];
+				node    = createItem(self, opt);
+			} else if (typeof renderDatas[i] === 'object' && !renderDatas[i].length) {
+				opt.src        = renderDatas[i][self.opts.imgName];
+				opt.href       = renderDatas[i][self.opts.hrefName];
+				opt.background = renderDatas[i][self.opts.backgroundName];
+				node = createItem(self, opt);
+			}
+
+			self.container.appendChild(node);
+		}
+	}
+
+	// 判断下标是否超出范围
+	var currentRange = function (crt, len) {
+		var current = parseInt(crt);
+		var length = parseInt(len);
+		current < 0 ? current = length - 1 : current;
+		current > length - 1 ? current = 0 : current;
+		return current;
+	}
+
+	// 上一个
+	var prev = function (self) {
+		var index = self.watchObj.current - 1;
+		index = currentRange(index, self.datas.length);
+
+		if (self.opts.animateType === 'slide') SlideAnimate(self, index);
+		if (self.opts.animateType === 'fade') FadeAnimate(self, index);
+	}
+
+	// 下一个
+	var next = function (self) {
+		var index = self.watchObj.current + 1;
+		index = currentRange(index, self.datas.length);
+
+		if (self.opts.animateType === 'slide') SlideAnimate(self, index);
+		if (self.opts.animateType === 'fade') FadeAnimate(self, index);
+	}
+
+	// 自动播放
+	var autoPlay = function (self) {
+		// 防止创建多个 计时器
+		if (!self.isAutoPlaying) {
+			var p = setInterval(function () {
+				next(self);
+			}, self.opts.speed + self.opts.animate);
+			self.player = p;
+
+			self.isAutoPlaying = true;
+			self.isAutoPlayStop = false;
+		}
+	}
+
+	// 停止播放
+	var stopPlay = function (self, setStopState) {
+		clearInterval(self.player);
+		self.isAutoPlaying = false;
+		!setStopState ? self.isAutoPlayStop = true : '';
+	}
+
+	// 重置计时器
+	var changeResetPlay = function (self) {
+		stopPlay(self, true);
+		var timer = setTimeout(function () {
+			clearTimeout(timer);
+			// 允许自动播放而且当前不是在停止状态
+			if (!self.isAutoPlayStop) autoPlay(self);
+		}, 300);
+	}
+
+	// 创建轮播分页
+	// @param  {object} pagerContainer 分页容器
+	// @param  {object} self           调用方的this
+	// @return {void}
+	var createPager = function (pagerContainer, self) {
+		if (!pagerContainer) {
+			var error = error('error', 'pager参数错误!未能选择到分页的容器。', self.errors);
+			return;
+		}
+
+		var left        = self.left;
+		var container   = self.container;
+		var current     = self.watchObj.current;
+		var animate     = self.opts.animate;
+		var animateType = self.opts.animateType;
+		var arr         = self.datas;
+		var pagerCtrl   = self.opts.pagerCtrl;
+		var active      = self.opts.pagerCon.active;
+		var pagerEvent  = self.opts.pagerCon.event;
+		var pagerDelay  = self.opts.pagerCon.delay;
+		var pagerStyle  = self.opts.pagerStyle;
+
+		// 分页点击事件
+		var change = function (current, e) {
+			var id;
+			if (!span.dataset) id = e.target.title;
+			else id = e.target.dataset.id;
+			var index = parseInt(id);
+
+			// 重置计数器
+			changeResetPlay(self);
+
+			var pagerSiblings = e.target.parentNode.childNodes;
+			for (var i = 0; i < pagerSiblings.length; i++) {
+				pagerSiblings[i].style.background = pagerStyle.background;
+			}
+			e.target.style.background = self.opts.pagerCon.active;
+
+			if (animateType === 'slide') SlideAnimate(self, index);
+			if (animateType === 'fade') FadeAnimate(self, index);
+		}
+
+		// 循环原始数组 一图对应一分页
+		for (var i = 0; i < arr.length; i++) {
+			var span = document.createElement('span');
+
+			// 分页样式
+			// pagerStyle
+			for (var style in pagerStyle) {
+				span.style[style] = pagerStyle[style];
+			}
+			span.className = 'carousel-pager';
+
+			// data- 兼容性
+			if (!span.dataset) span.title = i;
+			else span.dataset.id          = i;
+
+			// 初始化样式
+			current === i ? span.style.backgroundColor = active : '';
+
+			// 绑定分页控制事件
+			var timer;
+			if (pagerCtrl) span.addEventListener(pagerEvent, function (e) {
+				change(current, e);
+			});
+
+			// 将分页元素添加到分页容器
+			pagerContainer.appendChild(span);
+		}
+	}
+
+	// 设置分页样式
+	var setPager = function (pagerContainer, self) {
+		if (!pagerContainer) {
+			var error = error('error', 'pager参数错误!未能选择到分页的容器。', self.errorArr);
+			return;
+		}
+
+		var active     = self.opts.pagerCon.active;
+		var normal     = self.opts.pagerStyle.background;
+		var itemsPager = pagerContainer.childNodes;
+
+		for (var i = 0;i < itemsPager.length; i++) {
+			itemsPager[i].style.backgroundColor = normal;
+			if (self.watchObj.current === i) {
+				itemsPager[i].style.backgroundColor = active;
+			}
+		}
+	}
+
+	// 鼠标悬浮停止计时器
+	var hoverStopPlay = function (self) {
+		// 不能调用 stopPlay(self) 方法, 否则会立即执行
+		var stop = function () {
+			clearInterval(self.player);
+			self.isAutoPlaying = false;
+		}
+		self.container.addEventListener('mouseover', stop);
+		self.container.addEventListener('mouseleave', function () {
+			// 允许自动播放而且当前不是在停止状态
+			if (!self.isAutoPlayStop) {
+				autoPlay(self);
+			}
+		});
+	}
+
+	// 暴露公共方法 按钮
+	var publicBtnEvent = function (b, fn, fialMsg) {
+		if (!b) return;
+		fn  = fn || function () { };
+		msg = fialMsg || '获取按钮失败!请正确指定按钮的id或者classname!';
+		var btn, _this = this;
+		var error = function () {
+			console.warn(msg);
+			return;
+		}
+
+
+		btn = document.getElementById(b) || document.getElementsByClassName(b)[0];
+		btn ?
+		btn.addEventListener('click', function () {
+			fn();
+		}) :
+		error();
+	}
+
 	// 横向轮播类
 	// @param  {Object} options 轮播参数
 	// @return {void}           null
 	var Horizontal = function (options) {
-		this.opts = Init(options);
+		// 所有参数
+		this.opts             = Init(options);
+		// 轮播容器
+		this.container        = this.opts.container;
+		// 原始轮播数组
+		this.datas            = this.opts.datas;
+		// 初始化渲染数组 首插入最后一个元素 尾插入第一个元素
+		this.renderDatas      = this.opts.renderDatas;
+		// 错误数组
+		this.errors           = [];
+		// 监听对象
+		this.watchObj         = { _current: 0 };
+		// 倍数 即 100 、 渲染数组的长度
+		this._multiple        = 100  / this.renderDatas.length;
+		// 是否正在播放
+		this.isAutoPlaying    = false;
+		// 自动播放停止状态
+		this.isAutoPlayStop   = true;
+		// 自动播放计时器
+		this.player           = null;
 
-		console.log(this.opts);
+		// 赋值指定初始化current
+		this.watchObj.current = this.opts.current;
+		// 初始化偏移 不可从外部更改
+		this._initLeft        = -this._multiple * this.opts.overCount * (this.opts.current + 1);
+		// 计算偏移量
+		this.left             = this._initLeft;
+
+		var self              = this;
+		// container 参数
+		var boxOpt            = {};
+
+		boxOpt.height         = this.opts.height;
+		boxOpt.margin         = this.opts.margin;
+		boxOpt.padding        = this.opts.padding;
+		boxOpt.items          = this.opts.items;
+
+		// 处理轮播容器
+		setContainer(this.container, boxOpt);
+
+		// 初始化轮播容器
+		// 初始化总宽度
+		this.container.style.width     = this.renderDatas.length * 100 + '%';
+		// 初始化偏移量
+		//this.container.style.marginLeft = this.left + '%';
+		this.container.style.transform = 'translate3d(' + this.left + '%, 0, 0)';
+
+		// 创建元素
+		render(this);
+
+		// 初始化延时显示所有轮播
+		var timer = setTimeout(function () {
+			self.container.style.transition = 'opacity ' + self.opts.delay + 'ms ease-in';
+			self.container.style.visibility = 'visible';
+			self.container.style.opacity    = '1';
+			clearTimeout(timer);
+		}, 0);
+		var clearTimer = setTimeout(function () { self.container.style.transition = '';clearTimeout(clearTimer); }, this.opts.delay);
+
+		// 创建分页
+		var pagerContainer;
+		if (this.opts.isPager) {
+			pagerContainer = document.getElementById(this.opts.isPager) || document.getElementsByClassName(this.opts.isPager)[0];
+			createPager(pagerContainer ,this);
+		}
+
+		// 绑定上下按钮
+		this.prev(this.opts.prev);
+		this.next(this.opts.next);
+
+		// 自动播放
+		if (this.opts.isAuto) {
+			autoPlay(this);
+		}
+
+		// 鼠标悬浮停止
+		if (this.opts.hoverStop) {
+			hoverStopPlay(this);
+		}
+
+		// 监听 current index 的变化
+		Object.defineProperty(this.watchObj, "current", {
+		    get: function() {
+		        return this._current;
+		    },
+		    set: function(value) {
+		        if (value !== this._current) {
+		            this._current = value;
+		            // 设置分页样式
+		            if (pagerContainer) {
+						setPager(pagerContainer, self);
+					}
+		        }
+		    }
+		});
+	}
+
+	Horizontal.prototype.next = function (btn) {
+		var self    = this;
+		var success = function () {
+			// 重置计数器
+			changeResetPlay(self);
+			next(self);
+		}
+		publicBtnEvent(btn, success);
+	}
+
+	Horizontal.prototype.prev = function (btn) {
+		var self    = this;
+		var success = function () {
+			// 重置计数器
+			changeResetPlay(self);
+			prev(self);
+		}
+		publicBtnEvent(btn, success);
+	}
+
+	Horizontal.prototype.play = function (btn) {
+		var self    = this;
+		var success = function () {
+			autoPlay(self);
+		}
+		publicBtnEvent(btn, success);
+	}
+
+	Horizontal.prototype.stop = function (btn) {
+		var self    = this;
+		var success = function () {
+			stopPlay(self);
+		}
+		publicBtnEvent(btn, success);
 	}
 
 	// 纵向轮播类
 	// @param  {Object} options 轮播参数
 	// @return {void}           null
 	var Vertical = function (options) {
-		var opts = Init(options);
-
 		console.log(opts);
 		console.warn('%c Sorry, Vertical is Under development...', 'font-size: 20px;color: #ff5f00;');
 	}
@@ -173,11 +700,11 @@
 		 * @return {Object}         指定轮播的一个实例对象
 		 */
 		on: function (target, type, options) {
-			this.errorArr = [];
+			this.errors = [];
 			options       = options || {};
 
 			if (!target) {
-				var err = error('error', '请输入有效的容器id!', this.errorArr);
+				var err = error('error', '请输入有效的容器id!', this.errors);
 				console.error(err);
 				return this;
 			}
@@ -185,7 +712,7 @@
 			// 获取容器
 			var container = document.getElementById(target);
 			if (!container)  {
-				var err = error('error', '未能选择到轮播容器!请确定输入有效的id!', this.errorArr);
+				var err = error('error', '未能选择到轮播容器!请确定输入有效的id!', this.errors);
 				console.error(err);
 				return this;
 			}
@@ -196,14 +723,14 @@
 			// 验证数组是否合法
 			var dataArr = options.datas || [];
 			if (typeof dataArr !== 'object' || !dataArr.length || dataArr.length <= 0) {
-				var err = error('error', 'datas参数无效或错误!请确定datas值为长度大于0的数组!', this.errorArr);
+				var err = error('error', 'datas参数无效或错误!请确定datas值为长度大于0的数组!', this.errors);
 				console.error(err);
 				return this;
 			}
 
 			// 轮播类型验证不合法操作方法
 			var typeInvalid = function (self) {
-				var err = error('error', 'type参数可能无效!未能找到指定类型!', self.errorArr);
+				var err = error('error', 'type参数可能无效!未能找到指定类型!', self.errors);
 				console.error(err);
 				return self;
 			}
