372c46e69611e3d4bc4ebce54a77d6aa07adccac
Jehorn <gujh@bbdtek.com>
2019-04-16 17:58:37 +0800

    golang learn file

--- 01_learn/go-lang/helloworld.go
+++ 01_learn/go-lang/helloworld.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, Go!")
+
+	r := gin.Default()
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// 匹配restfull url参数
+	r.GET("/user/:name/*action", func(context *gin.Context) {
+		name := context.Param("name")
+		action := context.Param("action")
+		message := name + " is " + action
+		context.String(http.StatusOK, message)
+	})
+
+	// 获取URL参数
+	r.GET("/welcome", func(context *gin.Context) {
+		firstname := context.DefaultQuery("firstname", "Guest")
+		lastname := context.Query("lastname")
+
+		context.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
+	// Multipart/Urlencoded Form
+	r.POST("/form_post", func(context *gin.Context) {
+		message := context.PostForm("message")
+		nick := context.DefaultPostForm("nick", "anonymous")
+
+		context.JSON(http.StatusOK, gin.H{
+			"status": "posted",
+			"message": message,
+			"nick": nick,
+		})
+	})
+
+	// query + post form
+	r.POST("/post", func(context *gin.Context) {
+		id := context.Query("id")
+		page := context.DefaultQuery("page", "0")
+		name := context.PostForm("name")
+		message := context.PostForm("message")
+
+		context.JSON(http.StatusOK, gin.H{
+			"id": id,
+			"page": page,
+			"name": name,
+			"message": message,
+		})
+	})
+
+	// Map as querystring or postform parameters
+	r.POST("/map", func(context *gin.Context) {
+		ids := context.QueryMap("ids")
+		names := context.PostFormMap("names")
+
+		context.JSON(http.StatusOK, gin.H{
+			"ids": ids,
+			"names": names,
+		})
+	})
+
+	// upload files
+	r.POST("/upload", func(context *gin.Context) {
+		file, _ := context.FormFile("file")
+		log.Println(file.Filename)
+
+		context.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	r.Run(":8080")
+
 }
